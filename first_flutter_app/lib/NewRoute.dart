dc7293407e88f6c50c83a602cbb1c7256c11f64f
Vaith <vaithwee@yeah.net>
2019-09-11 10:54:32 +0800

    Added my first flutter app

---
+++ first_flutter_app/lib/NewRoute.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class NewRoute extends StatelessWidget {
+
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+          appBar: AppBar(
+              title: Text("New route"),
+          ),
+            body: Center(
+                child: Text("This is a new route"),
+            ),
+        );
+    }
+}
